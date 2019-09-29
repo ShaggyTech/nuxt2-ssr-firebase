ff63890b700103350a461f835f5e1f61e27e20dd
ShaggyTech <beedat1@gmail.com>
2019-09-28 22:45:17 -0500

    Fully functional PWA/SPA served from Firebase Hosting - with beginnings of SSR to PWA handoff

--- prod/server/index.js
+++ prod/server/index.js
@@ -1,28 +1,39 @@
-const functions = require("firebase-functions");
-const { Nuxt, Builder } = require("nuxt-edge");
-const express = require("express");
-const app = express();
+// process.env.DEBUG = 'nuxt:*'
+
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
+const compression = require('compression')
+const express = require('express')
+const app = express()
+app.use(compression())
+
+const nuxtConfig = require('./nuxt.config.js')
+
 const config = {
+  ...nuxtConfig,
   dev: false,
-  buildDir: "nuxt",
-  build: {
-    publicPath: "/assets/"
-  }
-};
-const nuxt = new Nuxt(config);
+  debug: false,
+  buildDir: 'nuxt'
+}
+const nuxt = new Nuxt(config)
 
-function handleRequest(req, res) {
-  console.log("IN New Nuxt Trial: ");
-  const isProduction = process.env.NODE_ENV === "development" ? false : true;
+async function handleRequest(req, res) {
+  const isProduction = process.env.NODE_ENV === 'development' ? false : true
   if (isProduction)
-    res.set("Cache-Control", "public, max-age=150, s-maxage=150");
+    res.set('Cache-Control', 'public, max-age=150, s-maxage=150')
 
-  try {
-    nuxt.render(req, res);
-  } catch (err) {
-    console.error(err);
-  }
+  // Render with Nuxt
+  // The only thing nuxt should be rendering is valid HTML
+  await nuxt
+    .renderRoute('/')
+    .then(({ html }) => {
+      res.send(html)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/404.html')
+    })
 }
 
-app.use(handleRequest);
-exports.nuxtssr = functions.https.onRequest(app);
+app.use(handleRequest)
+exports.nuxtssr = functions.https.onRequest(app)
